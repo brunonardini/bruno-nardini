7f2b9bfa9ba274d71063085105385e8653cf6bd9
Bruno Nardini <megatroom3@gmail.com>
2026-08-15 17:37:27 -0300

    ✨ Initialize project with ESLint, PostCSS, Tailwind CSS, and Next.js configuration
    - Added ESLint configuration for Next.js with custom global ignores.
    - Created Next.js configuration file.
    - Set up PostCSS with Tailwind CSS plugin.
    - Added global CSS styles for theming and typography.
    - Implemented basic layout structure with metadata and a navigation component.
    - Created a simple home page with a greeting message.
    - Included favicon and logo images.

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import { Navbar } from '@/components/navbar';
+import type { Metadata } from 'next';
+import { JetBrains_Mono, Lexend, Newsreader } from 'next/font/google';
+import './globals.css';
+
+const newsreader = Newsreader({
+  variable: '--font-newsreader',
+  subsets: ['latin', 'latin-ext'],
+  display: 'swap',
+  axes: ['opsz'],
+  style: ['normal', 'italic'],
+});
+
+const lexend = Lexend({
+  variable: '--font-lexend',
+  subsets: ['latin', 'latin-ext'],
+  display: 'swap',
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  variable: '--font-jetbrains-mono',
+  subsets: ['latin', 'latin-ext'],
+  display: 'swap',
+  style: ['normal', 'italic'],
+  preload: false,
+});
+
+export const metadata: Metadata = {
+  title: 'Bruno Nardini Blog',
+  description: 'Blog sobre desenvolvimento web e tecnologia',
+};
+
+export default function RootLayout({ children }: LayoutProps<'/'>) {
+  return (
+    <html
+      lang="pt-BR"
+      className={`${newsreader.variable} ${lexend.variable} ${jetbrainsMono.variable}`}
+    >
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </p>
+    </div>
+  );
+}
